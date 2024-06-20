6ef1d49100ab1d0c708d963e5fd4c90a9a675571
Onácio Santana <nsonacio@hotmail.com>
2024-06-19 22:34:04 -0300

    atualizacao

--- bfa.py
+++ bfa.py
@@ -5,16 +5,16 @@ import os
 class Bfa:
     def __init__(self):        
         self.lista_dados = {}        
-    
-    def consolidar(self, diretorio):        
-        arquivos = os.listdir(diretorio)        
-        unidades = {
+        self.unidades = {
             7209649: 'Sede I', 7209665: 'Sede II', 3912035: 'Maria Preta',
             5413958: 'Prudência Rosa', 2602016: 'Coqueiros', 2600692: 'Nagé',            
             2550156: 'Capanema', 2771551: 'São Roque I', 7168462: 'São Roque II',            
             7586175: 'Enseada', 9753508: 'Guapira', 3792714: 'Batatan',            
             3792676: 'Piedade', 7586183: 'Rio Grande',            
             }  
+    
+    def consolidar(self, diretorio):        
+        arquivos = os.listdir(diretorio)        
         
         for arquivo in arquivos:            
             arq, ext = os.path.splitext(arquivo)
@@ -40,11 +40,25 @@ class Bfa:
                 percentual = self.percentual(lista['Acompanhados'], total_beneficiarios)
                 lista['Percentual de acompanhamento'] = f'{percentual:.2f}'
             
-            self.lista_dados[unidades[unidade[0]]] = lista
+            self.lista_dados[self.unidades[unidade[0]]] = lista
                         
         return self.lista_dados
     
     def percentual(self, numerador, denominador):        
         percentual = (numerador / denominador) * 100
         return percentual
+    
+    def pegar_unidades_profissional(self, path, usf, prof):
+        arquivos = os.listdir(path)
+        for arquivo in arquivos:            
+            arq, ext = os.path.splitext(arquivo)
+
+            if ext == '.xls':
+                df = pd.read_html(path + '/' + arquivo)
+                usf1 = df[0]['CNES da EAS de vincula??o'].unique()
+                if usf in usf1:
+                    prof = df[0]['Nome do prof. de vincula??o'].unique()
+                    print(prof)
+    
+    
                 
--- main.py
+++ main.py
@@ -8,11 +8,30 @@ class JanelaPrincipal(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()        
         self.setupUi(self)
-        self.btn_relatorio.setEnabled(False)
+        self.cb_unidade.setEnabled(False)
+        self.cb_situacao.setEnabled(False)
+        self.cb_profissional.setEnabled(False)
+        self.btn_gerar_relatorio.setEnabled(False)
         self.btn_abrir.clicked.connect(self.abrir_arquivo)
+        self.btn_gerar_relatorio.clicked.connect(self.gerar_relatorio)
+
+        self.unidades = {
+            7209649: 'Sede I', 7209665: 'Sede II', 3912035: 'Maria Preta',
+            5413958: 'Prudência Rosa', 2602016: 'Coqueiros', 2600692: 'Nagé',            
+            2550156: 'Capanema', 2771551: 'São Roque I', 7168462: 'São Roque II',            
+            7586175: 'Enseada', 9753508: 'Guapira', 3792714: 'Batatan',            
+            3792676: 'Piedade', 7586183: 'Rio Grande',            
+            }  
+    def gerar_relatorio(self):
+        bfa = Bfa()
+        bfa.pegar_unidades_profissional(
+            self.lbl_caminho.text(), 
+            self.cb_unidade.currentText(), 
+            self.cb_profissional.currentText())
         
     def abrir_arquivo(self):        
         diretorio = QFileDialog.getExistingDirectory(self)
+        self.lbl_caminho.setText(diretorio)
         if diretorio:
             bfa = Bfa()
             dados = bfa.consolidar(diretorio)     
@@ -26,7 +45,16 @@ class JanelaPrincipal(QMainWindow, Ui_MainWindow):
                     self.listWidget.addItem(QListWidgetItem(f'-{ch} = {vl}'))
                 
                 self.listWidget.addItem(QListWidgetItem(f''))
-     
+        
+        for cnes, usf in self.unidades.items():            
+            self.cb_unidade.addItem(usf)
+        
+        self.cb_unidade.setEnabled(True)
+        self.cb_situacao.setEnabled(True)
+        self.cb_profissional.setEnabled(True)  
+        self.btn_gerar_relatorio.setEnabled(True)
+
+           
 
 if __name__ == '__main__':
     qt = QApplication(sys.argv)
